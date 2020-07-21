622235e57f618bbc49a67856007cf6d4f34203a1
Vikas Singh <vikas.singh@puresoftware.com>
2020-07-21 21:02:28 +0530

    soc: fsl: qbman: add QMan CCSR ACPI support
    Add ACPI support for the Queue Manager CCSR driver so that
    it can be probed in both DT and ACPI cases.
    
    Signed-off-by: Vikas Singh <vikas.singh@puresoftware.com>
    Signed-off-by: Camelia Groza <camelia.groza@nxp.com>
    Signed-off-by: Vladimir Oltean <vladimir.oltean@nxp.com>

--- drivers/soc/fsl/qbman/qman_ccsr.c
+++ drivers/soc/fsl/qbman/qman_ccsr.c
@@ -1,4 +1,5 @@
 /* Copyright 2008 - 2016 Freescale Semiconductor, Inc.
+ * Copyright 2020 Puresoftware Ltd.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -28,6 +29,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <linux/acpi.h>
 #include "qman_priv.h"
 
 u16 qman_ip_rev;
@@ -734,7 +736,6 @@ void qman_done_cleanup(void)
 	__qman_requires_cleanup = 0;
 }
 
-
 static int fsl_qman_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -752,6 +753,9 @@ static int fsl_qman_probe(struct platform_device *pdev)
 			node);
 		return -ENXIO;
 	}
+	dev_dbg(dev, "Qman IORESOURCE [%llx] of size [%llx]\n",
+		res->start, resource_size(res));
+
 	qm_ccsr_start = devm_ioremap(dev, res->start, resource_size(res));
 	if (!qm_ccsr_start)
 		return -ENXIO;
@@ -781,6 +785,7 @@ static int fsl_qman_probe(struct platform_device *pdev)
 		qm_channel_pool1 = QMAN_CHANNEL_POOL1_REV3;
 		qm_channel_caam = QMAN_CHANNEL_CAAM_REV3;
 	}
+	dev_dbg(dev, "Qman version:%04x,%02x,%02x\n", id, major, minor);
 
 	/*
 	* Order of memory regions is assumed as FQD followed by PFDR
@@ -884,10 +889,19 @@ static const struct of_device_id fsl_qman_ids[] = {
 	{}
 };
 
+#if IS_ENABLED(CONFIG_ACPI)
+static const struct acpi_device_id fsl_qman_acpi_ids[] = {
+	{"NXP0028", 0},
+	{}
+};
+MODULE_DEVICE_TABLE(acpi, fsl_qman_acpi_ids);
+#endif
+
 static struct platform_driver fsl_qman_driver = {
 	.driver = {
 		.name = KBUILD_MODNAME,
 		.of_match_table = fsl_qman_ids,
+		.acpi_match_table = ACPI_PTR(fsl_qman_acpi_ids),
 		.suppress_bind_attrs = true,
 	},
 	.probe = fsl_qman_probe,
