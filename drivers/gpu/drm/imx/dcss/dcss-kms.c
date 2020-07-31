7c80666a152f1aa25fa8e2fccf8c1157d72aa287
Laurentiu Palcu <laurentiu.palcu@nxp.com>
2020-07-31 13:31:59 +0300

    drm/imx/dcss: Add back component framework support
    Since HDP driver still uses it, add support back to DCSS driver until HDP gets
    rid of it.
    
    Signed-off-by: Laurentiu Palcu <laurentiu.palcu@nxp.com>
    [ Liu Ying: Fix conflicts to rebase upon next-20230119 ]
    Signed-off-by: Liu Ying <victor.liu@nxp.com>
    [ Liu Ying: Fix conflicts to rebase upon next-20231129 ]
    [ Aisheng: Fix conflicts to rebase upon next-20240220 ]
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/imx/dcss/dcss-kms.c
+++ drivers/gpu/drm/imx/dcss/dcss-kms.c
@@ -15,6 +15,7 @@
 #include <drm/drm_of.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vblank.h>
+#include <linux/component.h>
 
 #include "dcss-dev.h"
 #include "dcss-kms.h"
@@ -189,7 +190,7 @@ static int dcss_kms_bridge_connector_init(struct dcss_kms_dev *kms)
 	return 0;
 }
 
-struct dcss_kms_dev *dcss_kms_attach(struct dcss_dev *dcss)
+struct dcss_kms_dev *dcss_kms_attach(struct dcss_dev *dcss, bool componentized)
 {
 	struct dcss_kms_dev *kms;
 	struct drm_device *drm;
@@ -212,13 +213,21 @@ struct dcss_kms_dev *dcss_kms_attach(struct dcss_dev *dcss)
 	if (ret)
 		goto cleanup_mode_config;
 
-	ret = dcss_kms_bridge_connector_init(kms);
-	if (ret)
-		goto cleanup_mode_config;
+	if (!componentized) {
+		ret = dcss_kms_bridge_connector_init(kms);
+		if (ret)
+			goto cleanup_mode_config;
+	}
 
 	ret = dcss_crtc_init(crtc, drm);
 	if (ret)
 		goto cleanup_mode_config;
+
+	if (componentized) {
+		ret = component_bind_all(dcss->dev, kms);
+		if (ret)
+			goto cleanup_crtc;
+	}
 
 	drm_mode_config_reset(drm);
 
@@ -245,9 +254,10 @@ cleanup_mode_config:
 	return ERR_PTR(ret);
 }
 
-void dcss_kms_detach(struct dcss_kms_dev *kms)
+void dcss_kms_detach(struct dcss_kms_dev *kms, bool componentized)
 {
 	struct drm_device *drm = &kms->base;
+	struct dcss_dev *dcss = drm->dev_private;
 
 	drm_dev_unregister(drm);
 	drm_kms_helper_poll_fini(drm);
@@ -255,6 +265,8 @@ void dcss_kms_detach(struct dcss_kms_dev *kms)
 	drm_crtc_vblank_off(&kms->crtc.base);
 	drm_mode_config_cleanup(drm);
 	dcss_crtc_deinit(&kms->crtc, drm);
+	if (componentized)
+		component_unbind_all(dcss->dev, drm);
 	drm->dev_private = NULL;
 }
 
