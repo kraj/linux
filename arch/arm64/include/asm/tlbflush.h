b99193645ec3527975fae42bf10efd68644335a2
Li Yang <leoyang.li@nxp.com>
2023-08-14 15:22:21 -0500

    MLK-16005-2 arm64: tlb: add the SW workaround for i.MX8QM TKT340553
    on i.MX8QM 1.0/1.1,TLB maintenance through DVM messages over ARADDR channel,
    some bits (see the following) will be corrupted:
    
    ASID[15:12] VA[48:45] VA[44:41] VA[39:36]
    
    This issue will result in the TLB aintenance across the clusters not working
    as expected due to some VA and ASID bits get corrupted
    
    The SW workaround is: use the vmalle1is if VA larger than 36bits or
    ASID[15:12] is not zero, otherwise, we use original TLB maintenance path.
    
    Note: To simplify the code, we did not check VA[40] bit specifically
    
    As per latest i.MX8QM SOC Errata, TKT340553 workaround needs to be
    updated to unconditionally downgrade TLB operations and instruction
    cache maintenance.
    
    Current workaround is looping uselessly on the address
    range doing a  _tlbi(vmalle1is) which is harmful for
    the system performance and buggy as the instruction is
    flushing the entire TLB and there is no benefit of
    redoing it more than once. Also fix missing barriers.
    
    Since commit 967747bbc08 ("uaccess: remove CONFIG_SET_FS"), API
    user_addr_max() has been removed.  Change it to TASK_SIZE_MAX as done
    with other kernel code in upstream.
    
    Since commit bacac637025 ("arm64: extable: cleanup redundant extable type EX_TYPE_FIXUP")
    _ASM_EXTABLE is removed.  Change to use new API.
    
    [ Leo: split arch change from drivers/soc change, squashed all updates ]
    Signed-off-by: Jason Liu <jason.hui.liu@nxp.com>
    Signed-off-by: Nitin Garg <nitin.garg@nxp.com>
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Acked-by: Peng Fan <peng.fan@nxp.com>
    Signed-off-by: Marouen Ghodhbane <marouen.ghodhbane@nxp.com>
    Signed-off-by: Li Yang <leoyang.li@nxp.com>

--- arch/arm64/include/asm/tlbflush.h
+++ arch/arm64/include/asm/tlbflush.h
@@ -17,6 +17,8 @@
 #include <asm/cputype.h>
 #include <asm/mmu.h>
 
+extern bool TKT340553_SW_WORKAROUND;
+
 /*
  * Raw TLBI operations.
  *
@@ -276,9 +278,16 @@ static inline void flush_tlb_mm(struct mm_struct *mm)
 
 	dsb(ishst);
 	asid = __TLBI_VADDR(0, ASID(mm));
-	__tlbi(aside1is, asid);
-	__tlbi_user(aside1is, asid);
-	dsb(ish);
+	if (TKT340553_SW_WORKAROUND) {
+		/* Flush the entire TLB */
+		__tlbi(vmalle1is);
+		dsb(ish);
+		isb();
+	} else {
+		__tlbi(aside1is, asid);
+		__tlbi_user(aside1is, asid);
+		dsb(ish);
+	}
 	mmu_notifier_arch_invalidate_secondary_tlbs(mm, 0, -1UL);
 }
 
@@ -289,8 +298,15 @@ static inline void __flush_tlb_page_nosync(struct mm_struct *mm,
 
 	dsb(ishst);
 	addr = __TLBI_VADDR(uaddr, ASID(mm));
-	__tlbi(vale1is, addr);
-	__tlbi_user(vale1is, addr);
+	if (TKT340553_SW_WORKAROUND) {
+		/* Flush the entire TLB */
+		__tlbi(vmalle1is);
+		dsb(ish);
+		isb();
+	} else {
+		__tlbi(vale1is, addr);
+		__tlbi_user(vale1is, addr);
+	}
 	mmu_notifier_arch_invalidate_secondary_tlbs(mm, uaddr & PAGE_MASK,
 						(uaddr & PAGE_MASK) + PAGE_SIZE);
 }
@@ -452,6 +468,14 @@ static inline void __flush_tlb_range_nosync(struct mm_struct *mm,
 	dsb(ishst);
 	asid = ASID(mm);
 
+	if (TKT340553_SW_WORKAROUND) {
+		/* Flush the entire TLB and exit */
+		__tlbi(vmalle1is);
+		dsb(ish);
+		isb();
+		return;
+	}
+
 	if (last_level)
 		__flush_tlb_range_op(vale1is, start, pages, stride, asid,
 				     tlb_level, true, lpa2_is_enabled());
@@ -493,7 +517,8 @@ static inline void flush_tlb_kernel_range(unsigned long start, unsigned long end
 	end = round_up(end, stride);
 	pages = (end - start) >> PAGE_SHIFT;
 
-	if (__flush_tlb_range_limit_excess(start, end, pages, stride)) {
+	if (__flush_tlb_range_limit_excess(start, end, pages, stride)
+	    || (TKT340553_SW_WORKAROUND)) {
 		flush_tlb_all();
 		return;
 	}
@@ -514,7 +539,11 @@ static inline void __flush_tlb_kernel_pgtable(unsigned long kaddr)
 	unsigned long addr = __TLBI_VADDR(kaddr, 0);
 
 	dsb(ishst);
-	__tlbi(vaae1is, addr);
+	if (TKT340553_SW_WORKAROUND)
+		/* Flush the entire TLB */
+		__tlbi(vmalle1is);
+	else
+		__tlbi(vaae1is, addr);
 	dsb(ish);
 	isb();
 }
