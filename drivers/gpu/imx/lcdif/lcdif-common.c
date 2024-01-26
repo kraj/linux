8638bd3b24b0fa732d9bc93ec7845248cdb51b2e
Dong Aisheng <aisheng.dong@nxp.com>
2024-01-26 13:02:08 +0800

    drivers: fix build issues due to of_device.h and of_platform.h change
    Cope with upstream change:
    commit d57d584ef69d ("of: Stop circularly including of_device.h and of_platform.h")
    
    e.g.
    drivers/gpu/drm/panel/panel-raydium-rm67191.c:576:44: error: implicit declaration of function ‘of_match_device’; did you mean ‘i2c_of_match_device’? [-Werror=implicit-function-declaration]
      576 |         const struct of_device_id *of_id = of_match_device(rad_of_match, dev);
          |                                            ^~~~~~~~~~~~~~~
          |                                            i2c_of_match_device
    
    Signed-off-by: Dong Aisheng <aisheng.dong@nxp.com>

--- drivers/gpu/imx/lcdif/lcdif-common.c
+++ drivers/gpu/imx/lcdif/lcdif-common.c
@@ -20,6 +20,7 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/of_device.h>
+#include <linux/of_platform.h>
 #include <linux/of_graph.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
