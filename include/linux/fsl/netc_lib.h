a0d8987595fa0e415133867f4dc4646d337289df
Wei Fang <wei.fang@nxp.com>
2025-02-04 11:33:15 +0800

    net: enetc: add netc_show_tgst_entry() to netc-lib driver
    Both ENETC and NETC Switch support Time Gate Scheduling Table, sometimes
    debugging requires querying detailed information of the table entries.
    To facilitate debugging, netc_show_tgst_entry() is added as a generic
    function to netc-lib for ENETC driver and NETC Switch driver to use.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>

--- include/linux/fsl/netc_lib.h
+++ include/linux/fsl/netc_lib.h
@@ -6,7 +6,9 @@
 #ifndef __NETC_LIB_H
 #define __NETC_LIB_H
 
+#include <linux/debugfs.h>
 #include <linux/fsl/ntmp.h>
+#include <linux/seq_file.h>
 #include <net/pkt_sched.h>
 
 #if IS_ENABLED(CONFIG_NXP_NETC_LIB)
@@ -15,6 +17,18 @@ int netc_setup_taprio(struct ntmp_user *user, u32 entry_id,
 #else
 static inline int netc_setup_taprio(struct ntmp_user *user, u32 entry_id,
 				    struct tc_taprio_qopt_offload *f)
+{
+	return 0;
+}
+#endif
+
+#if IS_ENABLED(CONFIG_NXP_NETC_LIB) && IS_ENABLED(CONFIG_DEBUG_FS)
+/* debugfs API */
+int netc_show_tgst_entry(struct ntmp_user *user, struct seq_file *s,
+			 u32 entry_id);
+#else
+static inline int netc_show_tgst_entry(struct ntmp_user *user,
+				       struct seq_file *s, u32 entry_id)
 {
 	return 0;
 }
