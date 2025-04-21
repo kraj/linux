27772f54742ebe834af6441ff20af893d96e6565
Wei Fang <wei.fang@nxp.com>
2025-04-21 19:40:43 +0800

    net: enetc: add netc_read64() to netc_global.h
    Some registers of NETC are 64-bit, so add netc_read64() to read the
    value of these reigsters.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>

--- include/linux/fsl/netc_global.h
+++ include/linux/fsl/netc_global.h
@@ -11,6 +11,26 @@ static inline u32 netc_read(void __iomem *reg)
 	return ioread32(reg);
 }
 
+#ifdef ioread64
+static inline u64 netc_read64(void __iomem *reg)
+{
+	return ioread64(reg);
+}
+#else
+static inline u64 netc_read64(void __iomem *reg)
+{
+	u32 low, high;
+	u64 val;
+
+	low = ioread32(reg);
+	high = ioread32(reg + 4);
+
+	val = (u64)high << 32 | low;
+
+	return val;
+}
+#endif
+
 static inline void netc_write(void __iomem *reg, u32 val)
 {
 	iowrite32(val, reg);
