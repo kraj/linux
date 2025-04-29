c47c527e6166504e0cf4ee407609b5ca04df5502
Pankaj Gupta <pankaj.gupta@nxp.com>
2025-04-29 22:13:26 +0530

    LF-14885: drivers: firmware: imx: se: add ele-get-fw-version
    Adds a new API ELE_GET_FW_VERSION, to fetch the FW version and other
    misc. details.
    
    squash b1833efdd84ec TBS: drivers: firmware: imx: se: fix ELE Get FW version API
    
    Signed-off-by: Pankaj Gupta <pankaj.gupta@nxp.com>
    Signed-off-by: Rahul Kumar Yadav <rahulkumar.yadav@nxp.com>

--- drivers/firmware/imx/se_ctrl.c
+++ drivers/firmware/imx/se_ctrl.c
@@ -88,6 +88,8 @@ struct se_var_info {
 	uint8_t board_type;
 	u16 soc_id;
 	u16 soc_rev;
+	u32 fw_vers_word;
+	u32 commit_sha1;
 	struct se_fw_load_info load_fw;
 };
 
@@ -663,6 +665,20 @@ out:
 	return ret;
 }
 
+static bool runtime_fw_status(struct se_if_priv *priv)
+{
+	/*
+	 * Initializing to false as not sure if
+	 * the run-time FW status can be fetched for the SoC.
+	 */
+	bool fw_prsnt_n_running = false;
+
+	if (get_se_soc_id(priv) == SOC_ID_OF_IMX95 || get_se_soc_id(priv) == SOC_ID_OF_IMX94)
+		fw_prsnt_n_running =
+			(var_se_info.fw_vers_word & 0x1000000) ? true : false;
+
+	return fw_prsnt_n_running;
+}
 /*
  * get_se_soc_id() - to fetch the soc_id of the platform
  *
@@ -758,6 +774,11 @@ static int se_soc_info(struct se_if_priv *priv)
 			var_se_info.soc_id = soc_data->soc_id;
 			var_se_info.soc_rev = soc_data->soc_rev;
 		} else {
+			err = ele_get_fw_version(priv, &var_se_info.fw_vers_word,
+						 &var_se_info.commit_sha1);
+			if (err)
+				dev_err(priv->dev, "Failed to fetch FW version");
+
 			s_info = (void *)data;
 
 			var_se_info.board_type = 0;
@@ -881,6 +902,10 @@ static int se_load_firmware(struct se_if_priv *priv)
 	if (!ret)
 		load_fw->is_fw_loaded = true;
 
+	ret = ele_get_fw_version(priv, &var_se_info.fw_vers_word,
+				 &var_se_info.commit_sha1);
+	if (ret)
+		dev_err(priv->dev, "Failed to fetch FW version");
 exit:
 	return ret;
 }
@@ -2166,7 +2191,7 @@ static int se_if_probe(struct platform_device *pdev)
 			info_list->se_fw_img_nm.seco_fw_nm_in_rfs) {
 		load_fw = get_load_fw_instance(priv);
 		load_fw->se_fw_img_nm = &info_list->se_fw_img_nm;
-		load_fw->is_fw_loaded = false;
+		load_fw->is_fw_loaded = runtime_fw_status(priv);
 
 		if (info_list->se_fw_img_nm.prim_fw_nm_in_rfs) {
 			/* allocate buffer where SE store encrypted IMEM */
