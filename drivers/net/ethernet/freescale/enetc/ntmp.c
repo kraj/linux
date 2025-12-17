atmark-techno/linux-6.12-at

LF-15623-6 net: enetc: extract netc_enable_cbdr() as a helper function
When the switch or ENETC suspends, there is no need to free the command
BD ring's DMA buffer. This eliminates the need to re-allocate DMA memory
when resuming. Therefore, netc_enable_cbdr() is extracted from
netc_setup_cbdr() as a helper function to simplify the resume/suspend
process, and it will be used in a subsequent patch.

Another reason is that if switch or ENETC fails to resume, user space may
still have access to hardware resources. If the command BD ring's DMA
buffer is empty, accessing a null pointer could result in a crash.

Signed-off-by: Wei Fang <wei.fang@nxp.com>
Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>

drivers/net/ethernet/freescale/enetc/ntmp.c
@@ -65,6 +65,23 @@
 #define RSST_CFGE_DATA_SIZE(n)		(n)
 #define FMDT_DATA_LEN_ALIGN		4
 
+void netc_enable_cbdr(struct netc_cbdr *cbdr)
+{
+	cbdr->next_to_clean = netc_read(cbdr->regs.cir);
+	cbdr->next_to_use = netc_read(cbdr->regs.pir);
+
+	/* Step 1: Configure the base address of the Control BD Ring */
+	netc_write(cbdr->regs.bar0, lower_32_bits(cbdr->dma_base_align));
+	netc_write(cbdr->regs.bar1, upper_32_bits(cbdr->dma_base_align));
+
+	/* Step 2: Configure the number of BDs of the Control BD Ring */
+	netc_write(cbdr->regs.lenr, cbdr->bd_num);
+
+	/* Step 3: Enable the Control BD Ring */
+	netc_write(cbdr->regs.mr, NETC_CBDR_MR_EN);
+}
+EXPORT_SYMBOL_GPL(netc_enable_cbdr);
+
 int netc_setup_cbdr(struct device *dev, int cbd_num,
 		    struct netc_cbdr_regs *regs,
 		    struct netc_cbdr *cbdr)
@@ -91,18 +108,7 @@ int netc_setup_cbdr(struct device *dev, int cbd_num,
 
 	spin_lock_init(&cbdr->ring_lock);
 
-	cbdr->next_to_use = netc_read(cbdr->regs.pir);
-	cbdr->next_to_clean = netc_read(cbdr->regs.cir);
-
-	/* Step 1: Configure the base address of the Control BD Ring */
-	netc_write(cbdr->regs.bar0, lower_32_bits(cbdr->dma_base_align));
-	netc_write(cbdr->regs.bar1, upper_32_bits(cbdr->dma_base_align));
-
-	/* Step 2: Configure the number of BDs of the Control BD Ring */
-	netc_write(cbdr->regs.lenr, cbdr->bd_num);
-
-	/* Step 3: Enable the Control BD Ring */
-	netc_write(cbdr->regs.mr, NETC_CBDR_MR_EN);
+	netc_enable_cbdr(cbdr);
 
 	return 0;
 }

include/linux/fsl/ntmp.h
@@ -773,6 +773,7 @@ struct sbpt_query_data {
 };
 
 #if IS_ENABLED(CONFIG_NXP_NETC_LIB)
+void netc_enable_cbdr(struct netc_cbdr *cbdr);
 int netc_setup_cbdr(struct device *dev, int cbd_num, struct netc_cbdr_regs *regs,
 		    struct netc_cbdr *cbdr);
 void netc_teardown_cbdr(struct device *dev, struct netc_cbdr *cbdr);
@@ -863,6 +864,10 @@ int ntmp_fmdt_update_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
 int ntmp_fmdt_query_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
 			  u8 *data_buff, u32 data_len);
 #else
+static inline void netc_enable_cbdr(struct netc_cbdr *cbdr)
+{
+}
+
 static inline int netc_setup_cbdr(struct device *dev, int cbd_num,
 				  struct netc_cbdr_regs *regs,
 				  struct netc_cbdr *cbdr)

kraj/linux

drivers/net/ethernet/freescale/enetc/ntmp.c
@@ -66,6 +66,23 @@
 #define RSST_CFGE_DATA_SIZE(n)		(n)
 #define SGCLT_MAX_GE_NUM		256
 
+void ntmp_enable_cbdr(struct netc_cbdr *cbdr)
+{
+	cbdr->next_to_clean = netc_read(cbdr->regs.cir);
+	cbdr->next_to_use = netc_read(cbdr->regs.pir);
+
+	/* Step 1: Configure the base address of the Control BD Ring */
+	netc_write(cbdr->regs.bar0, lower_32_bits(cbdr->dma_base_align));
+	netc_write(cbdr->regs.bar1, upper_32_bits(cbdr->dma_base_align));
+
+	/* Step 2: Configure the number of BDs of the Control BD Ring */
+	netc_write(cbdr->regs.lenr, cbdr->bd_num);
+
+	/* Step 3: Enable the Control BD Ring */
+	netc_write(cbdr->regs.mr, NETC_CBDR_MR_EN);
+}
+EXPORT_SYMBOL_GPL(ntmp_enable_cbdr);
+
 int ntmp_init_cbdr(struct netc_cbdr *cbdr, struct device *dev,
 		   const struct netc_cbdr_regs *regs)
 {
@@ -90,18 +107,7 @@ int ntmp_init_cbdr(struct netc_cbdr *cbdr, struct device *dev,
 
 	spin_lock_init(&cbdr->ring_lock);
 
-	cbdr->next_to_use = netc_read(cbdr->regs.pir);
-	cbdr->next_to_clean = netc_read(cbdr->regs.cir);
-
-	/* Step 1: Configure the base address of the Control BD Ring */
-	netc_write(cbdr->regs.bar0, lower_32_bits(cbdr->dma_base_align));
-	netc_write(cbdr->regs.bar1, upper_32_bits(cbdr->dma_base_align));
-
-	/* Step 2: Configure the number of BDs of the Control BD Ring */
-	netc_write(cbdr->regs.lenr, cbdr->bd_num);
-
-	/* Step 3: Enable the Control BD Ring */
-	netc_write(cbdr->regs.mr, NETC_CBDR_MR_EN);
+	ntmp_enable_cbdr(cbdr);
 
 	return 0;
 }

include/linux/fsl/ntmp.h
@@ -714,6 +714,7 @@ struct sbpt_entry_data {
 };
 
 #if IS_ENABLED(CONFIG_NXP_NETC_LIB)
+void ntmp_enable_cbdr(struct netc_cbdr *cbdr);
 int ntmp_init_cbdr(struct netc_cbdr *cbdr, struct device *dev,
 		   const struct netc_cbdr_regs *regs);
 void ntmp_free_cbdr(struct netc_cbdr *cbdr);
@@ -804,6 +805,10 @@ static inline u32 ntmp_lookup_free_eid(unsigned long *bitmap, u32 size)
 }
 
 static inline void ntmp_clear_eid_bitmap(unsigned long *bitmap, u32 entry_id)
+{
+}
+
+static inline void ntmp_enable_cbdr(struct netc_cbdr *cbdr)
 {
 }
 