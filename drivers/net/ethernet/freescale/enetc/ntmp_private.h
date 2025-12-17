atmark-techno/linux-6.12-at

LF-14260-2 net: enetc: add VLAN filter table support
The VLAN filter table contains configuration and control information for
each VLAN configured on the switch. Each VLAN entry includes the VLAN
port membership, which FID to use in the FDB lookup, which spanning tree
group to use, the egress frame modification actions to apply to a frame
exiting form this VLAN, and various configuration and control parameters
for this VLAN.

Signed-off-by: Wei Fang <wei.fang@nxp.com>
Reviewed-by: Clark Wang <xiaoning.wang@nxp.com>

drivers/net/ethernet/freescale/enetc/ntmp_private.h
@@ -344,6 +344,36 @@ struct fdbt_resp_query {
 	u8 resv[3];
 };
 
+struct vft_ak_exact {
+	__le16 vid; /* bit0~11: VLAN ID, other bits are reserved */
+	__le16 resv;
+};
+
+union vft_access_key {
+	__le32 entry_id; /* entry_id match */
+	struct vft_ak_exact exact;
+	__le32 resume_entry_id; /* search */
+};
+
+struct vft_req_ua {
+	struct common_req_data crd;
+	union vft_access_key ak;
+	struct vft_cfge_data cfge;
+};
+
+struct vft_req_qd {
+	struct common_req_data crd;
+	union vft_access_key ak;
+};
+
+struct vft_resp_query {
+	__le32 status;
+	__le32 entry_id;
+	__le16 vid; /* KEYE_DATA */
+	__le16 resv;
+	struct vft_cfge_data cfge;
+};
+
 #pragma pack()
 
 struct tgst_query_data {

include/linux/fsl/ntmp.h
@@ -423,6 +423,27 @@ struct fdbt_acte_data {
 #define FDBT_ACT_FLAG		BIT(7)
 };
 
+struct vft_cfge_data {
+	__le32 bitmap_stg;
+#define	VFT_PORT_MEMBERSHIP	GENMASK(23, 0)
+#define	VFT_STG_ID_MASK		GENMASK(27, 24)
+#define VFT_STG_ID(g)		FIELD_PREP(VFT_STG_ID_MASK, (g))
+	__le16 fid;
+#define VFT_FID			GENMASK(11, 0)
+	__le16 cfg;
+#define VFT_MLO			GENMASK(2, 0)
+#define VFT_MFO			GENMASK(4, 3)
+#define VFT_IPMFE		BIT(6)
+#define VFT_IPMFLE		BIT(7)
+#define VFT_PGA			BIT(8)
+#define VFT_SFDA		BIT(10)
+#define VFT_OSFDA		BIT(11)
+#define VFT_FDBAFSS		BIT(12)
+	__le32 eta_port_bitmap;
+#define VFT_ETA_PORT_BITMAP	GENMASK(23, 0)
+	__le32 et_eid;
+};
+
 #pragma pack()
 
 struct netc_cbdr_regs {
@@ -449,6 +470,7 @@ struct netc_tbl_vers {
 	u8 rpt_ver;
 	u8 ipft_ver;
 	u8 fdbt_ver;
+	u8 vft_ver;
 	u8 isit_ver;
 	u8 ist_ver;
 	u8 isft_ver;
@@ -640,6 +662,15 @@ int ntmp_fdbt_delete_port_dynamic_entries(struct netc_cbdrs *cbdrs, int port);
 int ntmp_fdbt_search_port_entry(struct netc_cbdrs *cbdrs, int port,
 				u32 *resume_entry_id, u32 *entry_id,
 				struct fdbt_query_data *data);
+int ntmp_vft_add_entry(struct netc_cbdrs *cbdrs, u32 *entry_id,
+		       u16 vid, struct vft_cfge_data *cfge);
+int ntmp_vft_update_entry(struct netc_cbdrs *cbdrs, u16 vid,
+			  struct vft_cfge_data *cfge);
+int ntmp_vft_delete_entry(struct netc_cbdrs *cbdrs, u16 vid);
+int ntmp_vft_search_entry(struct netc_cbdrs *cbdrs, u32 *resume_eid,
+			  u32 *entry_id, u16 *vid, struct vft_cfge_data *cfge);
+int ntmp_vft_query_entry_by_vid(struct netc_cbdrs *cbdrs, u16 vid,
+				u32 *entry_id, struct vft_cfge_data *cfge);
 #else
 static inline int netc_setup_cbdr(struct device *dev, int cbd_num,
 				  struct netc_cbdr_regs *regs,
@@ -814,6 +845,37 @@ static inline int ntmp_fdbt_delete_port_dynamic_entries(struct netc_cbdrs *cbdrs
 static inline int ntmp_fdbt_search_port_entry(struct netc_cbdrs *cbdrs, int port,
 					      u32 *resume_entry_id, u32 *entry_id,
 					      struct fdbt_query_data *data)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_add_entry(struct netc_cbdrs *cbdrs, u32 *entry_id,
+				     u16 vid, struct vft_cfge_data *cfge)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_update_entry(struct netc_cbdrs *cbdrs, u16 vid,
+					struct vft_cfge_data *cfge)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_delete_entry(struct netc_cbdrs *cbdrs, u16 vid)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_search_entry(struct netc_cbdrs *cbdrs, u32 *resume_eid,
+					u32 *entry_id, u16 *vid,
+					struct vft_cfge_data *cfge)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_query_entry_by_vid(struct netc_cbdrs *cbdrs, u16 vid,
+					      u32 *entry_id,
+					      struct vft_cfge_data *cfge)
 {
 	return 0;
 }

kraj/linux

drivers/net/ethernet/freescale/enetc/ntmp_private.h
@@ -411,6 +411,36 @@ struct fdbt_resp_query {
 	u8 resv[3];
 };
 
+struct vft_ak_exact {
+	__le16 vid; /* bit0~11: VLAN ID, other bits are reserved */
+	__le16 resv;
+};
+
+union vft_access_key {
+	__le32 entry_id; /* entry_id match */
+	struct vft_ak_exact exact;
+	__le32 resume_entry_id; /* search */
+};
+
+struct vft_req_ua {
+	struct ntmp_cmn_req_data crd;
+	union vft_access_key ak;
+	struct vft_cfge_data cfge;
+};
+
+struct vft_req_qd {
+	struct ntmp_cmn_req_data crd;
+	union vft_access_key ak;
+};
+
+struct vft_resp_query {
+	__le32 status;
+	__le32 entry_id;
+	__le16 vid; /* KEYE_DATA */
+	__le16 resv;
+	struct vft_cfge_data cfge;
+};
+
 u32 ntmp_lookup_free_words(unsigned long *bitmap, u32 size, u32 num_words);
 void ntmp_clear_words_bitmap(unsigned long *bitmap, u32 entry_id, u32 num_words);
 int ntmp_tgst_query_entry(struct ntmp_user *user, u32 entry_id,

include/linux/fsl/ntmp.h
@@ -364,6 +364,27 @@ struct fdbt_acte_data {
 #define FDBT_ACT_FLAG		BIT(7)
 };
 
+struct vft_cfge_data {
+	__le32 bitmap_stg;
+#define	VFT_PORT_MEMBERSHIP	GENMASK(23, 0)
+#define	VFT_STG_ID_MASK		GENMASK(27, 24)
+#define VFT_STG_ID(g)		FIELD_PREP(VFT_STG_ID_MASK, (g))
+	__le16 fid;
+#define VFT_FID			GENMASK(11, 0)
+	__le16 cfg;
+#define VFT_MLO			GENMASK(2, 0)
+#define VFT_MFO			GENMASK(4, 3)
+#define VFT_IPMFE		BIT(6)
+#define VFT_IPMFLE		BIT(7)
+#define VFT_PGA			BIT(8)
+#define VFT_SFDA		BIT(10)
+#define VFT_OSFDA		BIT(11)
+#define VFT_FDBAFSS		BIT(12)
+	__le32 eta_port_bitmap;
+#define VFT_ETA_PORT_BITMAP	GENMASK(23, 0)
+	__le32 et_eid;
+};
+
 struct netc_cbdr_regs {
 	void __iomem *pir;
 	void __iomem *cir;
@@ -386,6 +407,7 @@ struct netc_tbl_vers {
 	u8 rpt_ver;
 	u8 ipft_ver;
 	u8 fdbt_ver;
+	u8 vft_ver;
 	u8 isit_ver;
 	u8 ist_ver;
 	u8 isft_ver;
@@ -569,6 +591,15 @@ int ntmp_fdbt_delete_port_dynamic_entries(struct ntmp_user *user, int port);
 int ntmp_fdbt_search_port_entry(struct ntmp_user *user, int port,
 				u32 *resume_entry_id, u32 *entry_id,
 				struct fdbt_entry_data *fdbt);
+int ntmp_vft_add_entry(struct ntmp_user *user, u32 *entry_id,
+		       u16 vid, struct vft_cfge_data *cfge);
+int ntmp_vft_update_entry(struct ntmp_user *user, u16 vid,
+			  struct vft_cfge_data *cfge);
+int ntmp_vft_delete_entry(struct ntmp_user *user, u16 vid);
+int ntmp_vft_search_entry(struct ntmp_user *user, u32 *resume_eid,
+			  u32 *entry_id, u16 *vid, struct vft_cfge_data *cfge);
+int ntmp_vft_query_entry_by_vid(struct ntmp_user *user, u16 vid,
+				u32 *entry_id, struct vft_cfge_data *cfge);
 #else
 static inline u32 ntmp_lookup_free_eid(unsigned long *bitmap, u32 size)
 {
@@ -723,6 +754,37 @@ static inline int ntmp_fdbt_delete_port_dynamic_entries(struct ntmp_user *user,
 static inline int ntmp_fdbt_search_port_entry(struct ntmp_user *user, int port,
 					      u32 *resume_entry_id, u32 *entry_id,
 					      struct fdbt_entry_data *fdbt)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_add_entry(struct ntmp_user *user, u32 *entry_id,
+				     u16 vid, struct vft_cfge_data *cfge)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_update_entry(struct ntmp_user *user, u16 vid,
+					struct vft_cfge_data *cfge)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_delete_entry(struct ntmp_user *user, u16 vid)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_search_entry(struct ntmp_user *user, u32 *resume_eid,
+					u32 *entry_id, u16 *vid,
+					struct vft_cfge_data *cfge)
+{
+	return 0;
+}
+
+static inline int ntmp_vft_query_entry_by_vid(struct ntmp_user *user, u16 vid,
+					      u32 *entry_id,
+					      struct vft_cfge_data *cfge)
 {
 	return 0;
 }