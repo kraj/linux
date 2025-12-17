atmark-techno/linux-6.12-at

LF-14260-5 net: enetc: add Egress Count Table support
Egress related statistics are maintained in this table. The Egress
Count table is implemented as a linear array of entries accessed
using an index (0, 1, 2, ..., n) that uniquely identifies an entry
within the array. Egress Counter Entry ID (EC_EID) is used as an
index to an entry in this table. The EC_EID is specified in the
Egress Treatment table. Egress Count table entries are always
present and enabled.

Signed-off-by: Wei Fang <wei.fang@nxp.com>
Reviewed-by: Clark Wang <xiaoning.wang@nxp.com>

include/linux/fsl/ntmp.h
@@ -490,6 +490,11 @@ struct esrt_srse_data {
 	__le32 sqr_history[4];
 };
 
+struct ect_stse_data {
+	__le64 enq_frm_cnt;
+	__le64 rej_frm_cnt;
+};
+
 #pragma pack()
 
 struct netc_cbdr_regs {
@@ -525,6 +530,7 @@ struct netc_tbl_vers {
 	u8 isct_ver;
 	u8 ett_ver;
 	u8 esrt_ver;
+	u8 ect_ver;
 };
 
 struct netc_cbdr {
@@ -734,6 +740,9 @@ int ntmp_esrt_update_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
 			   struct esrt_cfge_data *cfge);
 int ntmp_esrt_query_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
 			  struct esrt_query_data *data);
+int ntmp_ect_update_entry(struct netc_cbdrs *cbdrs, u32 entry_id);
+int ntmp_ect_query_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
+			 struct ect_stse_data *stse, bool update);
 #else
 static inline int netc_setup_cbdr(struct device *dev, int cbd_num,
 				  struct netc_cbdr_regs *regs,
@@ -968,6 +977,17 @@ static inline int ntmp_esrt_update_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
 
 static inline int ntmp_esrt_query_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
 					struct esrt_query_data *data)
+{
+	return 0;
+}
+
+static inline int ntmp_ect_update_entry(struct netc_cbdrs *cbdrs, u32 entry_id)
+{
+	return 0;
+}
+
+static inline int ntmp_ect_query_entry(struct netc_cbdrs *cbdrs, u32 entry_id,
+				       struct ect_stse_data *stse, bool update)
 {
 	return 0;
 }

kraj/linux

include/linux/fsl/ntmp.h
@@ -431,6 +431,11 @@ struct esrt_srse_data {
 	__le32 sqr_history[4];
 };
 
+struct ect_stse_data {
+	__le64 enq_frm_cnt;
+	__le64 rej_frm_cnt;
+};
+
 struct netc_cbdr_regs {
 	void __iomem *pir;
 	void __iomem *cir;
@@ -463,6 +468,7 @@ struct netc_tbl_vers {
 	u8 rfst_ver;
 	u8 ett_ver;
 	u8 esrt_ver;
+	u8 ect_ver;
 };
 
 struct netc_cbdr {
@@ -663,6 +669,9 @@ int ntmp_esrt_update_entry(struct ntmp_user *user, u32 entry_id,
 			   struct esrt_cfge_data *cfge);
 int ntmp_esrt_query_entry(struct ntmp_user *user, u32 entry_id,
 			  struct esrt_entry_data *erst);
+int ntmp_ect_update_entry(struct ntmp_user *user, u32 entry_id);
+int ntmp_ect_query_entry(struct ntmp_user *user, u32 entry_id,
+			 struct ect_stse_data *stse, bool update);
 #else
 static inline u32 ntmp_lookup_free_eid(unsigned long *bitmap, u32 size)
 {
@@ -877,6 +886,17 @@ static inline int ntmp_esrt_update_entry(struct ntmp_user *user, u32 entry_id,
 
 static inline int ntmp_esrt_query_entry(struct ntmp_user *user, u32 entry_id,
 					struct esrt_entry_data *erst)
+{
+	return 0;
+}
+
+static inline int ntmp_ect_update_entry(struct ntmp_user *user, u32 entry_id)
+{
+	return 0;
+}
+
+static inline int ntmp_ect_query_entry(struct ntmp_user *user, u32 entry_id,
+				       struct ect_stse_data *stse, bool update)
 {
 	return 0;
 }