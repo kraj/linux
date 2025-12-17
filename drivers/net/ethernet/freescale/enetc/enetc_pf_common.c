atmark-techno/linux-6.12-at

LF-14857-9 net: enetc: add AF_XDP zero copy support
Add AF_XDP Tx and Rx zero copy support for both ENETC v1 and v4, some
data tested by the xdpsock tool on i.MX95 board are as follows (the link
rate is 1Gbps):

1 Rx drop test
Copy mode:
./xdpsock -i eth0 -c -r
 sock0@eth0:0 rxdrop xdp-drv
                   pps            pkts           1.00
rx                 533023         533336

Zero copy mode:
./xdpsock -i eth0 -z -r
 sock0@eth0:0 rxdrop xdp-drv
                   pps            pkts           1.00
rx                 958063         958880

2 L2 forwarding
Copy mode:
./xdpsock -i eth0 -c -l
sock0@eth0:0 l2fwd xdp-drv
                   pps            pkts           1.00
rx                 371668         371904
tx                 371668         371904

Zero copy mode:
./xdpsock -i eth0 -z -l
sock0@eth0:0 l2fwd xdp-drv
                   pps            pkts           1.00
rx                 645606         646080
tx                 645606         646080

3 Tx only
Copy mode:
./xdpsock -i eth1 -t -c -s 64
 sock0@eth1:0 txonly xdp-drv
                   pps            pkts           1.00
tx                 594815         595584

Zero copy mode:
./xdpsock -i eth1 -t -z -s 64 -T 40
 sock0@eth1:0 txonly xdp-drv
                   pps            pkts           1.00
tx                 1363158        1364160

Comparing the results of the copy mode and the zero copy mode, we can
see that the performance of the zero copy mode is better than that of
the copy mode, which is consistent with our expectations.

In addition, jumbo frame (multi-buffer) is supported for both Rx and Tx.

Signed-off-by: Wei Fang <wei.fang@nxp.com>
Tested-by: Vladimir Oltean <vladimir.oltean@nxp.com>
Acked-by: Clark Wang <xiaoning.wang@nxp.com>

drivers/net/ethernet/freescale/enetc/enetc_pf_common.c
@@ -370,7 +370,8 @@ void enetc_pf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
 	ndev->priv_flags |= IFF_UNICAST_FLT;
 	ndev->xdp_features = NETDEV_XDP_ACT_BASIC | NETDEV_XDP_ACT_REDIRECT |
 			     NETDEV_XDP_ACT_NDO_XMIT | NETDEV_XDP_ACT_RX_SG |
-			     NETDEV_XDP_ACT_NDO_XMIT_SG;
+			     NETDEV_XDP_ACT_NDO_XMIT_SG |
+			     NETDEV_XDP_ACT_XSK_ZEROCOPY;
 
 	if (is_enetc_rev1(si)) {
 		priv->max_frags_bd = ENETC_MAX_SKB_FRAGS;
@@ -379,6 +380,8 @@ void enetc_pf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
 		priv->active_offloads |= ENETC_F_CHECKSUM;
 		priv->shared_tx_rings = true;
 	}
+
+	ndev->xdp_zc_max_segs = priv->max_frags_bd;
 
 	if (si->hw_features & ENETC_SI_F_RSC)
 		ndev->hw_features |= NETIF_F_LRO;

drivers/net/ethernet/freescale/enetc/enetc_vf.c
@@ -613,7 +613,10 @@ static void enetc_vf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
 
 	ndev->xdp_features = NETDEV_XDP_ACT_BASIC | NETDEV_XDP_ACT_REDIRECT |
 			     NETDEV_XDP_ACT_NDO_XMIT | NETDEV_XDP_ACT_RX_SG |
-			     NETDEV_XDP_ACT_NDO_XMIT_SG;
+			     NETDEV_XDP_ACT_NDO_XMIT_SG |
+			     NETDEV_XDP_ACT_XSK_ZEROCOPY;
+
+	ndev->xdp_zc_max_segs = priv->max_frags_bd;
 
 	/* If driver handles unicast address filtering, it should set
 	 * IFF_UNICAST_FLT in its priv_flags. (Refer to the description

kraj/linux

drivers/net/ethernet/freescale/enetc/enetc_pf_common.c
@@ -159,7 +159,10 @@ void enetc_pf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
 
 	ndev->xdp_features = NETDEV_XDP_ACT_BASIC | NETDEV_XDP_ACT_REDIRECT |
 			     NETDEV_XDP_ACT_NDO_XMIT | NETDEV_XDP_ACT_RX_SG |
-			     NETDEV_XDP_ACT_NDO_XMIT_SG;
+			     NETDEV_XDP_ACT_NDO_XMIT_SG |
+			     NETDEV_XDP_ACT_XSK_ZEROCOPY;
+
+	ndev->xdp_zc_max_segs = priv->max_frags;
 
 	if (!is_enetc_rev1(si)) {
 		ndev->features |= NETIF_F_HW_TC;

drivers/net/ethernet/freescale/enetc/enetc_vf.c
@@ -531,7 +531,10 @@ static void enetc_vf_netdev_setup(struct enetc_si *si, struct net_device *ndev,
 
 	ndev->xdp_features = NETDEV_XDP_ACT_BASIC | NETDEV_XDP_ACT_REDIRECT |
 			     NETDEV_XDP_ACT_NDO_XMIT | NETDEV_XDP_ACT_RX_SG |
-			     NETDEV_XDP_ACT_NDO_XMIT_SG;
+			     NETDEV_XDP_ACT_NDO_XMIT_SG |
+			     NETDEV_XDP_ACT_XSK_ZEROCOPY;
+
+	ndev->xdp_zc_max_segs = priv->max_frags;
 
 	if (si->num_rss) {
 		ndev->hw_features |= NETIF_F_RXHASH;