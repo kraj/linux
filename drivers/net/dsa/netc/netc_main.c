atmark-techno/linux-6.12-at

LF-14260-18 net: dsa: netc: add Time-Aware Scheduler support
The NETC Switch supports time based transmission mechanism, which is
implemented based on IEEE 802.1Qbv. Each traffic class has a gate, which
controls if the traffic class is open or closed. Only frames in traffic
classes with open gate are eligible for transmission. The gate state in
each traffic class is controlled by the gate control list. A gate control
list consists of an ordered list of gate control  entries, where each
entry specifies an 8 bit gate state vector where each bit (one per gate),
indicates whether the gate is open or closed. The gate control entry also
specifies the time duration for the states.

Example for TAPRIO with hardware offload:

tc qdisc replace dev swp0 parent root handle 200 taprio num_tc 8 \
map 0 1 2 3 4 5 6 7 queues 1@0 1@1 1@2 1@3 1@4 1@5 1@6 1@7 \
base-time 100000 sched-entry S ff 80000 sched-entry S 00 80000 \
max-sdu 1000 1000 1000 1000 1000 1000 1000 1000 flags 0x2

Signed-off-by: Wei Fang <wei.fang@nxp.com>
Reviewed-by: Clark Wang <xiaoning.wang@nxp.com>

drivers/net/dsa/netc/netc_main.c
@@ -473,6 +473,47 @@ static void netc_free_ntmp_bitmaps(struct netc_switch *priv)
 	ntmp->ett_eid_bitmap = NULL;
 }
 
+static u64 netc_switch_adjust_base_time(struct ntmp_priv *ntmp, u64 base_time,
+					u32 cycle_time)
+{
+	struct netc_switch *priv = ntmp_to_netc_switch(ntmp);
+	u32 tmr_devfn = priv->info->tmr_devfn;
+	u64 current_time, delta, n;
+	struct pci_dev *tmr_dev;
+	int domain;
+
+	domain = pci_domain_nr(priv->pdev->bus);
+	tmr_dev = pci_get_domain_bus_and_slot(domain, 0, tmr_devfn);
+	if (!tmr_dev)
+		return base_time;
+
+	current_time = netc_timer_get_current_time(tmr_dev);
+	if (base_time >= current_time)
+		return base_time;
+
+	delta = current_time - base_time;
+	n = DIV_ROUND_UP_ULL(delta, cycle_time);
+	base_time += (n * (u64)cycle_time);
+
+	return base_time;
+}
+
+static u32 netc_switch_get_tgst_free_words(struct ntmp_priv *ntmp)
+{
+	struct netc_switch *priv = ntmp_to_netc_switch(ntmp);
+	struct netc_switch_regs *regs = &priv->regs;
+	u32 words_in_use;
+	u32 total_words;
+
+	total_words = netc_base_rd(regs, NETC_TGSTCAPR);
+	total_words = NETC_GET_NUM_WORDS(total_words);
+
+	words_in_use = netc_base_rd(regs, NETC_TGSTMOR);
+	words_in_use = NETC_GET_NUM_WORDS(words_in_use);
+
+	return total_words - words_in_use;
+}
+
 static int netc_init_ntmp_priv(struct netc_switch *priv)
 {
 	struct ntmp_priv *ntmp = &priv->ntmp;
@@ -488,6 +529,9 @@ static int netc_init_ntmp_priv(struct netc_switch *priv)
 	err = netc_init_ntmp_bitmaps(priv);
 	if (err)
 		goto free_all_cbdrs;
+
+	ntmp->adjust_base_time = netc_switch_adjust_base_time;
+	ntmp->get_tgst_free_words = netc_switch_get_tgst_free_words;
 
 	return 0;
 
@@ -592,7 +636,7 @@ static void netc_port_set_tc_max_sdu(struct netc_port *port,
 	netc_port_wr(port, NETC_PTCTMSDUR(tc), val);
 }
 
-static void netc_port_set_all_tc_msdu(struct netc_port *port, u32 *max_sdu)
+void netc_port_set_all_tc_msdu(struct netc_port *port, u32 *max_sdu)
 {
 	u32 msdu = NETC_MAX_FRAME_LEN;
 	int tc;
@@ -1868,6 +1912,8 @@ static int netc_port_setup_tc(struct dsa_switch *ds, int port_id,
 		return netc_tc_setup_mqprio(priv, port_id, type_data);
 	case TC_SETUP_QDISC_CBS:
 		return netc_tc_setup_cbs(priv, port_id, type_data);
+	case TC_SETUP_QDISC_TAPRIO:
+		return netc_tc_setup_taprio(priv, port_id, type_data);
 	default:
 		return -EOPNOTSUPP;
 	}

drivers/net/dsa/netc/netc_switch.h
@@ -66,6 +66,7 @@
 struct netc_switch_info {
 	u32 cpu_port_num;
 	u32 usr_port_num;
+	u32 tmr_devfn;
 	void (*phylink_get_caps)(int port, struct phylink_config *config);
 };
 
@@ -78,6 +79,7 @@ struct netc_port_caps {
 enum netc_port_offloads {
 	NETC_FLAG_QAV			= BIT(0),
 	NETC_FLAG_QBU			= BIT(1),
+	NETC_FLAG_QBV			= BIT(2),
 };
 
 struct netc_switch;
@@ -182,8 +184,12 @@ struct netc_vlan_entry {
 #define netc_glb_rd(r, o)		netc_read((r)->global + (o))
 #define netc_glb_wr(r, o, v)		netc_write((r)->global + (o), v)
 
+#define ntmp_to_netc_switch(ntmp_priv)	\
+	container_of((ntmp_priv), struct netc_switch, ntmp)
+
 int netc_switch_platform_probe(struct netc_switch *priv);
 void netc_port_set_tx_pause(struct netc_port *port, bool tx_pause);
+void netc_port_set_all_tc_msdu(struct netc_port *port, u32 *max_sdu);
 
 /* TC APIs */
 int netc_tc_query_caps(struct tc_query_caps_base *base);
@@ -191,6 +197,8 @@ int netc_tc_setup_mqprio(struct netc_switch *priv, int port,
 			 struct tc_mqprio_qopt_offload *mqprio);
 int netc_tc_setup_cbs(struct netc_switch *priv, int port,
 		      struct tc_cbs_qopt_offload *cbs);
+int netc_tc_setup_taprio(struct netc_switch *priv, int port,
+			 struct tc_taprio_qopt_offload *taprio);
 
 /* ethtool APIs */
 void netc_port_mm_commit_preemptible_tcs(struct netc_port *port);

drivers/net/dsa/netc/netc_tc.c
@@ -16,6 +16,13 @@ int netc_tc_query_caps(struct tc_query_caps_base *base)
 
 		return 0;
 	}
+	case TC_SETUP_QDISC_TAPRIO: {
+		struct tc_taprio_caps *caps = base->caps;
+
+		caps->supports_queue_max_sdu = true;
+
+		return 0;
+	}
 	default:
 		return -EOPNOTSUPP;
 	}
@@ -150,8 +157,12 @@ static int netc_port_setup_cbs(struct netc_port *port,
 
 		netc_port_set_tc_cbs_params(port, tc, false, 0);
 
-		if (tc == top_prio_tc)
+		if (tc == top_prio_tc) {
+			if (!(port->offloads & NETC_FLAG_QBV))
+				netc_port_enable_time_gating(port, false);
+
 			port->offloads &= ~NETC_FLAG_QAV;
+		}
 
 		return 0;
 	}
@@ -202,4 +213,112 @@ int netc_tc_setup_cbs(struct netc_switch *priv, int port_id,
 		      struct tc_cbs_qopt_offload *cbs)
 {
 	return netc_port_setup_cbs(priv->ports[port_id], cbs);
+}
+
+static bool netc_port_get_tge_status(struct netc_port *port)
+{
+	u32 val;
+
+	val = netc_port_rd(port, NETC_PTGSCR);
+	if (val & PTGSCR_TGE)
+		return true;
+
+	return false;
+}
+
+static int netc_port_setup_taprio(struct netc_port *port,
+				  struct tc_taprio_qopt_offload *taprio)
+{
+	struct netc_switch *priv = port->switch_priv;
+	u32 entry_id = port->index;
+	bool tge;
+	int err;
+
+	/* Set the maximum frame size for each traffic class */
+	netc_port_set_all_tc_msdu(port, taprio->max_sdu);
+
+	tge = netc_port_get_tge_status(port);
+	if (!tge)
+		netc_port_enable_time_gating(port, true);
+
+	err = netc_setup_taprio(&priv->ntmp, entry_id, taprio);
+	if (err)
+		goto disable_time_gating;
+
+	port->offloads |= NETC_FLAG_QBV;
+
+	return 0;
+
+disable_time_gating:
+	if (!tge)
+		netc_port_enable_time_gating(port, false);
+
+	netc_port_set_all_tc_msdu(port, NULL);
+
+	return err;
+}
+
+static int netc_tc_taprio_replace(struct netc_switch *priv, int port_id,
+				  struct tc_taprio_qopt_offload *taprio)
+{
+	struct netc_port *port = NETC_PORT(priv, port_id);
+	struct netlink_ext_ack *extack = taprio->extack;
+	int err;
+
+	err = netc_tc_setup_mqprio(priv, port_id, &taprio->mqprio);
+	if (err) {
+		NL_SET_ERR_MSG_MOD(extack, "Setup mqprio failed");
+		return err;
+	}
+
+	err = netc_port_setup_taprio(port, taprio);
+	if (err)
+		netc_port_reset_mqprio(port);
+
+	return err;
+}
+
+static int netc_port_reset_taprio(struct netc_port *port)
+{
+	/* Remove both operational and administrative gate control list from
+	 * the corresponding table entry by disabling time gate scheduling on
+	 * the port.
+	 */
+	netc_port_enable_time_gating(port, false);
+
+	/* Time gate scheduling should be enabled for the port if credit-based
+	 * shaper is going to be used in combination with frame preemption.
+	 */
+	if (port->offloads & NETC_FLAG_QAV && port->offloads & NETC_FLAG_QBU)
+		netc_port_enable_time_gating(port, true);
+
+	/* Reset TC max SDU */
+	netc_port_set_all_tc_msdu(port, NULL);
+
+	port->offloads &= ~NETC_FLAG_QBV;
+
+	return 0;
+}
+
+static int netc_tc_taprio_destroy(struct netc_switch *priv, int port_id)
+{
+	struct netc_port *port = NETC_PORT(priv, port_id);
+
+	netc_port_reset_taprio(port);
+	netc_port_reset_mqprio(port);
+
+	return 0;
+}
+
+int netc_tc_setup_taprio(struct netc_switch *priv, int port_id,
+			 struct tc_taprio_qopt_offload *taprio)
+{
+	switch (taprio->cmd) {
+	case TAPRIO_CMD_REPLACE:
+		return netc_tc_taprio_replace(priv, port_id, taprio);
+	case TAPRIO_CMD_DESTROY:
+		return netc_tc_taprio_destroy(priv, port_id);
+	default:
+		return -EOPNOTSUPP;
+	}
 }

kraj/linux

drivers/net/dsa/netc/netc_main.c
@@ -456,12 +456,66 @@ static void netc_free_ntmp_bitmaps(struct netc_switch *priv)
 	user->ett_gid_bitmap = NULL;
 }
 
+static struct pci_dev *netc_get_ptp_timer(struct netc_switch *priv)
+{
+	struct pci_bus *bus = priv->pdev->bus;
+	u32 devfn = priv->info->tmr_devfn;
+
+	return pci_get_domain_bus_and_slot(pci_domain_nr(bus),
+					   bus->number, devfn);
+}
+
+static u64 netc_switch_adjust_base_time(struct ntmp_user *user, u64 base_time,
+					u32 cycle_time)
+{
+	struct netc_switch *priv = ntmp_to_netc_switch(user);
+	u64 current_time, delta, n;
+	struct pci_dev *tmr_dev;
+
+	tmr_dev = netc_get_ptp_timer(priv);
+	if (!tmr_dev)
+		return base_time;
+
+	current_time = netc_timer_get_current_time(tmr_dev);
+	pci_dev_put(tmr_dev);
+	if (base_time >= current_time)
+		return base_time;
+
+	delta = current_time - base_time;
+	n = DIV_ROUND_UP_ULL(delta, cycle_time);
+	base_time += (n * (u64)cycle_time);
+
+	return base_time;
+}
+
+static u32 netc_switch_get_tgst_free_words(struct ntmp_user *user)
+{
+	struct netc_switch *priv = ntmp_to_netc_switch(user);
+	struct netc_switch_regs *regs = &priv->regs;
+	u32 words_in_use;
+	u32 total_words;
+
+	total_words = netc_base_rd(regs, NETC_TGSTCAPR);
+	total_words = NETC_GET_NUM_WORDS(total_words);
+
+	words_in_use = netc_base_rd(regs, NETC_TGSTMOR);
+	words_in_use = NETC_GET_NUM_WORDS(words_in_use);
+
+	return total_words - words_in_use;
+}
+
+static const struct ntmp_ops ntmp_ops = {
+	.adjust_base_time = netc_switch_adjust_base_time,
+	.get_tgst_free_words = netc_switch_get_tgst_free_words,
+};
+
 static int netc_init_ntmp_user(struct netc_switch *priv)
 {
 	struct ntmp_user *user = &priv->user;
 	int err;
 
 	user->dev_type = NETC_DEV_SWITCH;
+	user->ops = &ntmp_ops;
 	netc_init_ntmp_tbl_versions(priv);
 	netc_get_ntmp_capabilities(priv);
 
@@ -578,7 +632,7 @@ static void netc_port_set_tc_max_sdu(struct netc_port *port,
 	netc_port_wr(port, NETC_PTCTMSDUR(tc), val);
 }
 
-static void netc_port_set_all_tc_msdu(struct netc_port *port, u32 *max_sdu)
+void netc_port_set_all_tc_msdu(struct netc_port *port, u32 *max_sdu)
 {
 	u32 overhead = ETH_FCS_LEN + VLAN_ETH_HLEN;
 	int tc;
@@ -1839,6 +1893,8 @@ static int netc_port_setup_tc(struct dsa_switch *ds, int port_id,
 		return netc_tc_setup_mqprio(priv, port_id, type_data);
 	case TC_SETUP_QDISC_CBS:
 		return netc_tc_setup_cbs(priv, port_id, type_data);
+	case TC_SETUP_QDISC_TAPRIO:
+		return netc_tc_setup_taprio(priv, port_id, type_data);
 	default:
 		return -EOPNOTSUPP;
 	}

drivers/net/dsa/netc/netc_switch.h
@@ -60,6 +60,7 @@ struct netc_port;
 struct netc_switch_info {
 	u32 cpu_port_num;
 	u32 usr_port_num;
+	u32 tmr_devfn;
 	void (*phylink_get_caps)(int port, struct phylink_config *config);
 	void (*bpt_init)(struct netc_switch *priv);
 	void (*port_tx_pause_config)(struct netc_port *port, bool tx_pause);
@@ -74,6 +75,7 @@ struct netc_port_caps {
 enum netc_port_offloads {
 	NETC_FLAG_QAV			= BIT(0),
 	NETC_FLAG_QBU			= BIT(1),
+	NETC_FLAG_QBV			= BIT(2),
 };
 
 struct netc_port {
@@ -177,8 +179,12 @@ struct netc_vlan_entry {
 #define netc_glb_rd(r, o)		netc_read((r)->global + (o))
 #define netc_glb_wr(r, o, v)		netc_write((r)->global + (o), v)
 
+#define ntmp_to_netc_switch(ntmp_user)	\
+	container_of((ntmp_user), struct netc_switch, user)
+
 int netc_switch_platform_probe(struct netc_switch *priv);
 void netc_port_set_tx_pause(struct netc_port *port, bool tx_pause);
+void netc_port_set_all_tc_msdu(struct netc_port *port, u32 *max_sdu);
 
 /* TC APIs */
 int netc_tc_query_caps(struct tc_query_caps_base *base);
@@ -186,6 +192,8 @@ int netc_tc_setup_mqprio(struct netc_switch *priv, int port,
 			 struct tc_mqprio_qopt_offload *mqprio);
 int netc_tc_setup_cbs(struct netc_switch *priv, int port,
 		      struct tc_cbs_qopt_offload *cbs);
+int netc_tc_setup_taprio(struct netc_switch *priv, int port,
+			 struct tc_taprio_qopt_offload *taprio);
 
 /* ethtool APIs */
 void netc_port_mm_commit_preemptible_tcs(struct netc_port *port);

drivers/net/dsa/netc/netc_tc.c
@@ -16,6 +16,13 @@ int netc_tc_query_caps(struct tc_query_caps_base *base)
 
 		return 0;
 	}
+	case TC_SETUP_QDISC_TAPRIO: {
+		struct tc_taprio_caps *caps = base->caps;
+
+		caps->supports_queue_max_sdu = true;
+
+		return 0;
+	}
 	default:
 		return -EOPNOTSUPP;
 	}
@@ -150,8 +157,12 @@ static int netc_port_setup_cbs(struct netc_port *port,
 
 		netc_port_set_tc_cbs_params(port, tc, false, 0);
 
-		if (tc == top_prio_tc)
+		if (tc == top_prio_tc) {
+			if (!(port->offloads & NETC_FLAG_QBV))
+				netc_port_enable_time_gating(port, false);
+
 			port->offloads &= ~NETC_FLAG_QAV;
+		}
 
 		return 0;
 	}
@@ -202,4 +213,112 @@ int netc_tc_setup_cbs(struct netc_switch *priv, int port_id,
 		      struct tc_cbs_qopt_offload *cbs)
 {
 	return netc_port_setup_cbs(priv->ports[port_id], cbs);
+}
+
+static bool netc_port_get_tge_status(struct netc_port *port)
+{
+	u32 val;
+
+	val = netc_port_rd(port, NETC_PTGSCR);
+	if (val & PTGSCR_TGE)
+		return true;
+
+	return false;
+}
+
+static int netc_port_setup_taprio(struct netc_port *port,
+				  struct tc_taprio_qopt_offload *taprio)
+{
+	struct netc_switch *priv = port->switch_priv;
+	u32 entry_id = port->index;
+	bool tge;
+	int err;
+
+	/* Set the maximum frame size for each traffic class */
+	netc_port_set_all_tc_msdu(port, taprio->max_sdu);
+
+	tge = netc_port_get_tge_status(port);
+	if (!tge)
+		netc_port_enable_time_gating(port, true);
+
+	err = netc_setup_taprio(&priv->user, entry_id, taprio);
+	if (err)
+		goto disable_time_gating;
+
+	port->offloads |= NETC_FLAG_QBV;
+
+	return 0;
+
+disable_time_gating:
+	if (!tge)
+		netc_port_enable_time_gating(port, false);
+
+	netc_port_set_all_tc_msdu(port, NULL);
+
+	return err;
+}
+
+static int netc_tc_taprio_replace(struct netc_switch *priv, int port_id,
+				  struct tc_taprio_qopt_offload *taprio)
+{
+	struct netc_port *port = NETC_PORT(priv, port_id);
+	struct netlink_ext_ack *extack = taprio->extack;
+	int err;
+
+	err = netc_tc_setup_mqprio(priv, port_id, &taprio->mqprio);
+	if (err) {
+		NL_SET_ERR_MSG_MOD(extack, "Setup mqprio failed");
+		return err;
+	}
+
+	err = netc_port_setup_taprio(port, taprio);
+	if (err)
+		netc_port_reset_mqprio(port);
+
+	return err;
+}
+
+static int netc_port_reset_taprio(struct netc_port *port)
+{
+	/* Remove both operational and administrative gate control list from
+	 * the corresponding table entry by disabling time gate scheduling on
+	 * the port.
+	 */
+	netc_port_enable_time_gating(port, false);
+
+	/* Time gate scheduling should be enabled for the port if credit-based
+	 * shaper is going to be used in combination with frame preemption.
+	 */
+	if (port->offloads & NETC_FLAG_QAV && port->offloads & NETC_FLAG_QBU)
+		netc_port_enable_time_gating(port, true);
+
+	/* Reset TC max SDU */
+	netc_port_set_all_tc_msdu(port, NULL);
+
+	port->offloads &= ~NETC_FLAG_QBV;
+
+	return 0;
+}
+
+static int netc_tc_taprio_destroy(struct netc_switch *priv, int port_id)
+{
+	struct netc_port *port = NETC_PORT(priv, port_id);
+
+	netc_port_reset_taprio(port);
+	netc_port_reset_mqprio(port);
+
+	return 0;
+}
+
+int netc_tc_setup_taprio(struct netc_switch *priv, int port_id,
+			 struct tc_taprio_qopt_offload *taprio)
+{
+	switch (taprio->cmd) {
+	case TAPRIO_CMD_REPLACE:
+		return netc_tc_taprio_replace(priv, port_id, taprio);
+	case TAPRIO_CMD_DESTROY:
+		return netc_tc_taprio_destroy(priv, port_id);
+	default:
+		return -EOPNOTSUPP;
+	}
 }