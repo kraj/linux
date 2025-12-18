atmark-techno/linux-6.12-at

LF-15623-9 net: dsa: netc: restore tc-taprio when switch resumes
If the NETC is powered off in suspend mode, the tc-taprio configuration
will be lost. Therefore, when the system resumes, we need to restore
the tc-taprio configuration to ensure that tc-taprio can still resume
working after the system resumes.

Note that the current patch only supports restoring the last set gate
control list, while the time gate sheduling table (TGST) has two lists,
an administrative gate control list and an operational gate control
list. Therefore, if both lists are valid, only the administrative gate
control list can be restored. So the current patch can be further
improved, but this will undoubtedly make the driver more complicated,
and perhaps it will be improved in the future.

Signed-off-by: Wei Fang <wei.fang@nxp.com>
Reviewed-by: Vladimir Oltean <vladimir.oltean@nxp.com>

drivers/net/dsa/netc/netc_pm.c
@@ -227,20 +227,49 @@ static void netc_remove_fdbt_entries(struct netc_switch *priv)
 	mutex_unlock(&priv->fdbt_lock);
 }
 
+static int netc_port_restore_taprio(struct netc_port *port)
+{
+	struct netc_switch *priv = port->switch_priv;
+	u32 entry_id = port->index;
+
+	if (!port->taprio)
+		return 0;
+
+	return netc_setup_taprio(&priv->ntmp, entry_id, port->taprio);
+}
+
 static int netc_port_restore_config(struct netc_port *port)
 {
+	int err;
+
 	if (!port->dp)
 		return 0;
 
 	netc_port_fixed_config(port);
 
-	return netc_port_restore_config_from_db(port);
+	err = netc_port_restore_config_from_db(port);
+	if (err)
+		return err;
+
+	err = netc_port_restore_taprio(port);
+	if (err)
+		goto del_ptp_filter;
+
+	return 0;
+
+del_ptp_filter:
+	netc_port_set_ptp_filter(port, HWTSTAMP_FILTER_NONE);
+
+	return err;
 }
 
 static void netc_port_remove_config(struct netc_port *port)
 {
 	if (!port->dp)
 		return;
+
+	if (port->taprio)
+		netc_port_reset_taprio(port);
 
 	netc_port_set_ptp_filter(port, HWTSTAMP_FILTER_NONE);
 }

kraj/linux

drivers/net/dsa/netc/netc_pm.c
@@ -227,20 +227,49 @@ static void netc_remove_fdbt_entries(struct netc_switch *priv)
 	mutex_unlock(&priv->fdbt_lock);
 }
 
+static int netc_port_restore_taprio(struct netc_port *port)
+{
+	struct netc_switch *priv = port->switch_priv;
+	u32 entry_id = port->index;
+
+	if (!port->taprio)
+		return 0;
+
+	return netc_setup_taprio(&priv->user, entry_id, port->taprio);
+}
+
 static int netc_port_restore_config(struct netc_port *port)
 {
+	int err;
+
 	if (!port->dp)
 		return 0;
 
 	netc_port_fixed_config(port);
 
-	return netc_port_restore_config_from_db(port);
+	err = netc_port_restore_config_from_db(port);
+	if (err)
+		return err;
+
+	err = netc_port_restore_taprio(port);
+	if (err)
+		goto del_ptp_filter;
+
+	return 0;
+
+del_ptp_filter:
+	netc_port_set_ptp_filter(port, HWTSTAMP_FILTER_NONE);
+
+	return err;
 }
 
 static void netc_port_remove_config(struct netc_port *port)
 {
 	if (!port->dp)
 		return;
+
+	if (port->taprio)
+		netc_port_reset_taprio(port);
 
 	netc_port_set_ptp_filter(port, HWTSTAMP_FILTER_NONE);
 }